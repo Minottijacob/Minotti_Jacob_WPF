0004166627af3486a3bc099a6d9e9efbf077ff11
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-10 17:20:35 -0400

    Prompt User for Name
    Prompt User for Name

--- GoToWeek2/GoToTrainingWeekTwo/public_html/js/Script.js
+++ GoToWeek2/GoToTrainingWeekTwo/public_html/js/Script.js
@@ -7,3 +7,12 @@ Sept. 10, 2014
 
 //alert("Lok'Tar Ogar, Friend! Your JavaScript has been successfully added!");
 
+// Create a calculator for age
+// Ask the user their name
+var name= prompt("Please enter in your name.");
+
+//test
+console.log(name);
+
+//alerting user
+alert("Welcome "+ name +"! Let's figure out how old you are!");
