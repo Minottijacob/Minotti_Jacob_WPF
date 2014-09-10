9e15788490b9912fbed082813bb61714db65caab
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-10 16:36:42 -0400

    Completed JavaScript Tutorials
    Completed JavaScript Tutorials on Variables and Arrays.

---
+++ Variables/HTML5Application1/public_html/js/Script.js
@@ -0,0 +1,25 @@
+/* Jacob Minotti
+ * Sept. 10th, 2014
+ * Defining Variables
+ */
+
+alert("Noodlecup!");
+
+var age; //declaration
+age = 19; //deinition
+
+//storage container = value
+
+var yearOfBirth = 1995; //declaration
+
+
+var IsStudent = true; //Boolean Variable is either true or false no caps
+
+var FirstName = "Jacob"; //Quotes needed for strings.
+
+var phrase = 'I don\'t like the Alliance' // \ can be used to tell the machine to ignore the statement such as an '
+
+//Arrays
+
+var HordeRaces = ["Orc", "Undead", "Tauren", "Goblin", "Blood Elf", "Pandaren", "Troll" ];
+console.log(HordeRaces[2]);
