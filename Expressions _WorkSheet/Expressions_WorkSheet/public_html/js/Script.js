04034bf7a3ef91bc9f531af810112d1617191847
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 12:50:14 -0400

    Completed Pizza Problem
    Completed Pizza Problem

--- Expressions _WorkSheet/Expressions_WorkSheet/public_html/js/Script.js
+++ Expressions _WorkSheet/Expressions_WorkSheet/public_html/js/Script.js
@@ -14,3 +14,12 @@ var totalDogYears= dogYears * humanYears;
 //Alerting user of Sparky's age in Dog years
 
 alert("Hello Friend! According to my calculations, Sparky is " + humanYears + " which is " + totalDogYears + " in dog years!");
+
+//Now setting up for the pizza problem
+var sliceOfPizza = prompt("How many slices of pizza is there to one pie? ");
+var partyPeople = prompt("How many people where at the party?");
+var pizzaOrdered = prompt("How many pizzas were ordered?");
+var slicePerPerson = pizzaOrdered * sliceOfPizza/partyPeople;
+//Now solving problem based off of numbers imputed by user
+
+alert("Greetings, Pizzabot. According to my calculations you should get " + slicePerPerson +" pieces of pizza at the party!");
