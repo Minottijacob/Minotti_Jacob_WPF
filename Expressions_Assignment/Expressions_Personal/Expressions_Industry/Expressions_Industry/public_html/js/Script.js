763d2b872cf19af25082bb9d46da0791e5b0a907
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 16:11:26 -0400

    Created Variables
    Created Variables for Web Devs with an array.

--- Expressions_Assignment/Expressions_Personal/Expressions_Industry/Expressions_Industry/public_html/js/Script.js
+++ Expressions_Assignment/Expressions_Personal/Expressions_Industry/Expressions_Industry/public_html/js/Script.js
@@ -4,5 +4,10 @@ Sept. 11th, 2014
 Expressions: Industy
  */
 
-alert("Lok'Tar Ogar, friend! You have successfully linked your JavaScript into your HTML file. You may now begin coding.");
+//alert("Lok'Tar Ogar, friend! You have successfully linked your JavaScript into your HTML file. You may now begin coding.");
 
+//Setting up for success- I want to calculate the average amount of Developers I should put on my company's next project.
+
+var currentDevTeams = [4, 6, 4, 8, 3, 5, 6];
+var total = currentDevTeams[0] + currentDevTeams[1] + currentDevTeams[2] + currentDevTeams[3] + currentDevTeams [4] + currentDevTeams[5] + currentDevTeams[6];
+console.log(total);
