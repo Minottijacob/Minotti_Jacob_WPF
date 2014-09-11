7fedb8fb3c3f0eb2e77f3a1489da19549ab3c6a4
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 16:41:11 -0400

    Commented out the test alert.
    Commented out the test alert.

--- Expressions_Assignment/Expressions_Wacky/Expressions_Wacky/public_html/js/Script.js
+++ Expressions_Assignment/Expressions_Wacky/Expressions_Wacky/public_html/js/Script.js
@@ -4,7 +4,9 @@ Sept. 11th, 2014
 Expression: Wacky
  */
 
-//How many limes are actually in that coconut?
+//alert ("You put the lime in the coconut.");
+
+//How many limes are actually in that coconut before you shake it all up?
 var name = prompt ("Hello user. Tell me what's your name?");
 
 
