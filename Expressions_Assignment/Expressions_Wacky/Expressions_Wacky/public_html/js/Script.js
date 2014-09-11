e3c3e2f2924555f209217512fbc12f3ed78c4477
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 16:52:51 -0400

    Asks for user's name and If Else Statement
    Asks for user's name and If Else Statement

--- Expressions_Assignment/Expressions_Wacky/Expressions_Wacky/public_html/js/Script.js
+++ Expressions_Assignment/Expressions_Wacky/Expressions_Wacky/public_html/js/Script.js
@@ -8,5 +8,9 @@ Expression: Wacky
 
 //How many limes are actually in that coconut before you shake it all up?
 var name = prompt ("Hello user. Tell me what's your name?");
-
-
+alert ("Greetings " + name + "!");
+var limes = prompt ("Do you know how many limes are in the coconut before you shake it all up? Yes or No?");
+// If Else Statement
+if (limes === "Yes") {
+   var limesInCoconut = prompt ("You do! How many?");
+}
