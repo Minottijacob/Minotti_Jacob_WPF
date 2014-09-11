8fb080e1797917569aec6a89a4bb426647824ac6
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 12:25:33 -0400

    Setup Variable for Question 1
    Setup Variable for Question 1

---
+++ Expressions _WorkSheet/Expressions_WorkSheet/public_html/js/Script.js
@@ -0,0 +1,12 @@
+/* Jacob Minotti
+ * Expressions Worksheet
+ * Sept. 11th, 2014
+ */
+
+//alert("Lok'Tar Ogar, Friend! Your JavaScript has been successfully added!");
+
+
+//Setting up Variables
+var dogYears = 7;
+var humanYears = 1;
+var totalDogYears= dogYears * humanYears;
