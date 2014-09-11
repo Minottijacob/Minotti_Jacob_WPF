59abf2a8b6e76b89750014eace9ebe56de927604
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 13:30:20 -0400

    Setup Variables for Personal Expressions
    Setup Variables for Personal Expressions.

--- Expressions_Assignment/Expressions_Personal/Expressions_Personal/public_html/js/Script.js
+++ Expressions_Assignment/Expressions_Personal/Expressions_Personal/public_html/js/Script.js
@@ -5,3 +5,9 @@ Expressions: Personal
  */
 alert("Lok'Tar Ogar, Friend! You have successfully setup your JavaScript files.");
 
+//Setting up Variables
+var raidDays = prompt("How many days this week do you plan on raiding?");
+var hoursDevoted = prompt("How many hours do you plan on raiding this week?");
+var daysWasted = raidDays *hoursDevoted/24;
+console.log(daysWasted);
+//Alerting user their answer
