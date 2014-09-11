48b153e6328d4e2c20e1faafefb14c58b552e734
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 14:22:11 -0400

    Setup JavaScript files for second project.
    Setup JavaScript files for second project.

---
+++ Expressions_Assignment/Expressions_Personal/Expressions_Industry/Expressions_Industry/public_html/js/Script.js
@@ -0,0 +1,8 @@
+/* 
+ Jacob Minotti
+Sept. 11th, 2014
+Expressions: Industy
+ */
+
+alert("Lok'Tar Ogar, friend! You have successfully linked your JavaScript into your HTML file. You may now begin coding.");
+
