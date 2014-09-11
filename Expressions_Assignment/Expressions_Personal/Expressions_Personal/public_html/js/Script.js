0b5510b983983c2990ea6caaceab88bc64bf745d
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-11 13:46:43 -0400

    Prompted user for name
    Prompted user for name and addressed the purpose of this program.

--- Expressions_Assignment/Expressions_Personal/Expressions_Personal/public_html/js/Script.js
+++ Expressions_Assignment/Expressions_Personal/Expressions_Personal/public_html/js/Script.js
@@ -3,9 +3,12 @@ Jacob Minotti
 Sept. 11th, 2014
 Expressions: Personal
  */
-alert("Lok'Tar Ogar, Friend! You have successfully setup your JavaScript files.");
+//alert("Lok'Tar Ogar, Friend! You have successfully setup your JavaScript files.");
 
-//Setting up Variables
+//Greeting users, addressing my purpose
+var name = prompt("Please enter your name in the text box below.");
+alert("Greetings, " + name + ". Let's find out how many days you're going to waste raiding in World of Warcraft this week!");
+//Setting up Variable
 var raidDays = prompt("How many days this week do you plan on raiding?");
 var hoursDevoted = prompt("How many hours do you plan on raiding this week?");
 var daysWasted = raidDays *hoursDevoted/24;
