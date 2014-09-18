b12b5567d757131bd129dbafbc63e051d20e2496
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-18 14:46:42 -0400

    Started conditionals.
    Started conditionals.

--- Assignment_Conditionals/Conditionals_Personal/public_html/js/Script.js
+++ Assignment_Conditionals/Conditionals_Personal/public_html/js/Script.js
@@ -4,6 +4,16 @@ Sept. 18th, 2014
 Conditionals: Personal
  */
 
-alert ("Lok'Tar Ogar, Friend! If you can see this message you have successfully attached you JavaScript file into your HTML and you're ready to code!");
+//alert ("Lok'Tar Ogar, Friend! If you can see this message you have successfully attached you JavaScript file into your HTML and you're ready to code!");
 
 //Homework or World of Warcraft
+
+var userInput = prompt ("Do you have assignments due this week?");
+
+if (userInput === "Yes"){
+    alert ("You should probably work on your assignments.");
+  
+}
+else {
+    var userElseInput = prompt 
+}
