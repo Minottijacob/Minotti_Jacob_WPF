fac7b2d16f5509bdddc3b0a861d67da7aaa87608
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-18 19:06:44 -0400

    Added in a Validation Conditional.
    Added in a Validation Conditional.

--- Assignment_Conditionals/Conditionals_Industry/public_html/js/Script.js
+++ Assignment_Conditionals/Conditionals_Industry/public_html/js/Script.js
@@ -11,13 +11,17 @@ Conditionals: Industry
 var coworkers = prompt ("How many other coworkers do you have in your department? (minimum of 5)");
 var coworkersOnVacation = prompt ("How many coworkers are on vacation?");
 var vacation;
+//Testing for any missing values
 
-
+if (coworkers === "" || coworkersOnVacation === ""){
+    alert ("I'm sorry. I didn't see a response.");
+}
+else {
 //Now evaluating to see if this lucky person can go on vacation or not.
 
 vacation= (coworkersOnVacation > coworkers - 3) ? "I'm sorry but you can not go on vacation this week."  : "Yes! You may go on vacation.";
 
 //Now alerting user their response
 alert (vacation);
-
+}
 //console.log (coworkersOnVacation > coworkers - 3);
