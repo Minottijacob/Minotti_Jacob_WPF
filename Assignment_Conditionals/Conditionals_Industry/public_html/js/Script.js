67c67c713dc23c5ee330faa5796000103e79878f
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-18 18:41:06 -0400

    Setting up Variables for Industry Project.
    Setting up Variables for Industry Project.

--- Assignment_Conditionals/Conditionals_Industry/public_html/js/Script.js
+++ Assignment_Conditionals/Conditionals_Industry/public_html/js/Script.js
@@ -4,4 +4,10 @@ Sept. 18th 2014
 Conditionals: Industry
  */
 
-alert ("Well met friend! You have successfully embedded your JavaScript into your HTML! You may begin coding!");
+//alert ("Well met friend! You have successfully embedded your JavaScript into your HTML! You may begin coding!");
+
+//Employees and vacation time
+
+var coworkers = prompt ("How many other coworkers do you have in your department?");
+var coworkersOnVacation = prompt ("How many coworkers are on vacation?");
+
