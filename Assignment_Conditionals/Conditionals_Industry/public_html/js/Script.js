63cfb03ced9aee9f2d322a50cfc6a3e97b857dcb
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-18 18:55:23 -0400

    Testing Equation in Conditional via Console.log
    Testing Equation in Conditional via Console.log

--- Assignment_Conditionals/Conditionals_Industry/public_html/js/Script.js
+++ Assignment_Conditionals/Conditionals_Industry/public_html/js/Script.js
@@ -12,10 +12,12 @@ var coworkers = prompt ("How many other coworkers do you have in your department
 var coworkersOnVacation = prompt ("How many coworkers are on vacation?");
 var vacation;
 
+
 //Now evaluating to see if this lucky person can go on vacation or not.
 
-vacation= (coworkers >= coworkersOnVacation) ? "Yes! You may go on vacation." : "I'm sorry you may not go on vacation this week.";
+//vacation= (coworkersOnVacation > coworkers - 3) ? "Yes! You may go on vacation." : "I'm sorry you may not go on vacation this week.";
 
 //Now alerting user their response
 alert (vacation);
 
+console.log (coworkersOnVacation > coworkers - 3);
