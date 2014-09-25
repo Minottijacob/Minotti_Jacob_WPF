dcf64015c275e542a393d42db511fbdebb9fe931
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-25 18:08:05 -0400

    Created Logical Operators for Is This Patrick
    Created Logical Operators for Is This Patrick

--- Functions_Assignment/Functions_Wacky/public_html/js/Script.js
+++ Functions_Assignment/Functions_Wacky/public_html/js/Script.js
@@ -4,5 +4,18 @@ Sept. 25th, 2014
 Functions: Wacky
  */
 
-alert ("Lok'Tar Ogar");
+     //alert ("Lok'Tar Ogar");
+var isThis = prompt ("Is this the Krusty Krab? Yes or No.");
+var patrick = prompt ("Is your name Patrick Star? Yes or No.");
 
+function krabby (isThis, patrick) {
+    if (isThis === "No" && patrick === "Yes" ) {
+        alert ("This is Patrick.");
+    }
+    else {
+        alert ("Hi! May I take your order?");
+    };
+
+}
+
+krabby(isThis, patrick);
