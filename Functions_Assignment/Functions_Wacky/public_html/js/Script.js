a3ccc50680b5c611dc00e81ef5dc6e198005e7ee
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-25 17:46:52 -0400

    Setup JavaScript Embedded in the HTML
    Setup JavaScript Embedded in the HTML

--- Functions_Assignment/Functions_Wacky/public_html/js/Script.js
+++ Functions_Assignment/Functions_Wacky/public_html/js/Script.js
@@ -1,7 +1,8 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Jacob R. Minotti
+Sept. 25th, 2014
+Functions: Wacky
  */
 
+alert ("Lok'Tar Ogar");
 
