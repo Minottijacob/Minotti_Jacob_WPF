8a94fd5296254ca3649efe7fee8aed0bf18cea83
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-25 15:26:47 -0400

    Setting Up Function
    Setting Up Function

--- Functions_Assignment/Functions_Personal/public_html/js/Script.js
+++ Functions_Assignment/Functions_Personal/public_html/js/Script.js
@@ -4,4 +4,10 @@ Sept 25th, 2014
 Functions : Personal
  */
 
-alert ("Lok'Tar Ogar.");
+//alert ("Lok'Tar Ogar.");
+var userPrompt = prompt ("How much gold do you currently have in World of Warcraft?");
+
+var getMount = function characterGold() {
+    (userPrompt > 600000) ? "You can purchase the Spectral Tiger mount" : "I'm sorry you have a little more saving to do.";
+};
+alert (getMount); 
