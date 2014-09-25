2eed7e7a699892548509ca4f26b40a6a8095fd6a
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-25 13:40:35 -0400

    Embedded JavaScript in HTML File Successfully
    Embedded JavaScript in HTML File Successfully

--- Functions_Assignment/Functions_Personal/public_html/js/Script.js
+++ Functions_Assignment/Functions_Personal/public_html/js/Script.js
@@ -1,7 +1,7 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Jacob R. Minotti
+Sept 25th, 2014
+Functions : Personal
  */
 
-
+alert ("Lok'Tar Ogar.");
