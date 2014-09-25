e93843c2e0acc8db7b6a54a7ca1528e28d7cee27
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-25 15:58:04 -0400

    Fixed my Issues with Functions
    Fixed my Issues with Functions and came out with a working product. I
    realize now that I messed up with the data storage part.

--- Functions_Assignment/Functions_Personal/public_html/js/Script.js
+++ Functions_Assignment/Functions_Personal/public_html/js/Script.js
@@ -6,10 +6,13 @@ Functions : Personal
 
 //alert ("Lok'Tar Ogar.");
 
-var getMount = function characterGold() {
-return (userPrompt > 600000) ? "You can purchase the Spectral Tiger mount" : "I'm sorry you have a little more saving to do.";
-
+var goldCalc = function(gold, cost){
+        var getMount = (gold >= cost) ? "You may purchase the Spectral Tiger mount!" : "I'm sorry you have a little more saving to do.";
+    return getMount;
 };
-var userPrompt = prompt ("How much gold do you currently have in World of Warcraft?");
+//Setting up some var
+var gold = prompt ("How much gold do you have in World of Warcraft?");
+var cost = prompt ("How much gold does the Spectral Tiger mount cost on your server?");
+var mount = goldCalc(gold, cost);
 
-console.log (getMount);
+alert (mount);
