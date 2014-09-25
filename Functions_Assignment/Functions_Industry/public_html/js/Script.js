3911ccc4eaab7c357164c99ea2aa4891aca71668
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-09-25 17:25:14 -0400

    Created a Function for Due Dates
    Created a Function for Due Dates

--- Functions_Assignment/Functions_Industry/public_html/js/Script.js
+++ Functions_Assignment/Functions_Industry/public_html/js/Script.js
@@ -4,4 +4,24 @@ Sept. 25th, 2014
 Functions: Industry
  */
 
-alert ("Lok'Tar Ogar");
+//alert ("Lok'Tar Ogar");
+
+//Project 
+var time = prompt ("What day is it?");
+var expected = prompt ("What day is your project due?");
+
+
+function project(time, expected) {
+    if (time > expected) {
+        alert ("You still have time to work on the project.");
+    }
+    else if (time === expected) {
+        alert ("Did you finish the project?");
+    }
+    else {
+        alert ("Your project is overdue.");
+    }
+};
+
+project(time, expected);
+
